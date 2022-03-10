9d0f64dac842facb34faf0a74440b8f0acb8b665
em432 <emejeothniel@gmail.com>
2022-03-10 23:16:54 +0000

    My final commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main - prints the standrad error 
+ *
+ * Return: returns 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
