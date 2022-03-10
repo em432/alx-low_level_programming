7662a2f1967c8dfced69b398212cf582afad31ea
em432 <emejeothniel@gmail.com>
2022-03-10 23:21:36 +0000

    My commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<unistd.h>
 /**
- * main - prints the standrad error 
+ * main - prints the standrad error sandwiched around the  standard outpu/t
  *
  * Return: returns 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
