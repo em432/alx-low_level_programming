dfb7d7501050be89a7e8bf388f8de8c157c30650
em432 <emejeothniel@gmail.com>
2022-03-10 17:02:36 +0000

    My commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
