31cfe3253622901bf567eefe91dda1a400defa93
em432 <emejeothniel@gmail.com>
2022-03-10 17:13:04 +0000

    My commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
