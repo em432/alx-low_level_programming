73a0d3d7f9fe083c221c0a419b32077164c9adcf
em432 <emejeothniel@gmail.com>
2022-03-11 16:49:33 +0000

    My commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/corect)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
