d5fefcddfc548c78ef26d990179bb80be37a17ee
em432 <emejeothniel@gmail.com>
2022-03-11 16:41:59 +0000

    mycommit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int digit;
+
+for (digit = 0 ; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+
+printf("\n");
+
+return (0);
+
+}
