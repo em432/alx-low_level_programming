acd07a4ab24204e931425b3437228ecb7e7c596b
em432 <emejeothniel@gmail.com>
2022-03-11 16:54:34 +0000

    my commit message

---
+++ 0x01-variables_if_else_while/7-print_tebahpa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
