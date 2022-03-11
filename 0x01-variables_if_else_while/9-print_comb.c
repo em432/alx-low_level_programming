ac5b656527d65160b8f6dae37c67d2b4b7ecea6c
em432 <emejeothniel@gmail.com>
2022-03-11 17:14:01 +0000

    My commit message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point 
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int number;
+
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
