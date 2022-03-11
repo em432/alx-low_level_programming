b1c3531c956d1a3c3716593612e6759312c5e487
em432 <emejeothniel@gmail.com>
2022-03-11 16:23:42 +0000

    My commit message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess/correct)
+ */
+int main(void)
+
+{
+char lower_case;
+char upper_case;
+
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+
+for (lower_case = 'a'; lower_case <= 'z': lower_case++)
+{
+putchar(lower_case);
+}
+
+
+putchar('\n');
+
+return (0);
+
+}
