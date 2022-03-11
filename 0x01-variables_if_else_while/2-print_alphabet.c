5600b40763e4914c97bc0bf4c800ddbb3e866af0
em432 <emejeothniel@gmail.com>
2022-03-11 16:13:26 +0000

    my commit message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
