2180d043625305e200fbc70884b14180d4776025
em432 <emejeothniel@gmail.com>
2022-03-12 02:09:12 +0000

    my commit message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/completed)
+ */
+int main(void)
+
+{
+int number_left;
+int number_right;
+
+
+for (number_left = 48; number_right <= 57; number_left++)
+{
+for (number_right = number_left + 1 ; number_right <= 57; number_right++)
+{
+
+putchar(number_left);
+putchar (number_right);
+
+
+if ((number_left == 56) && (number_right == 57))
+{
+break;
+}
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+}
+
+putchar('\n');
+
+
+return (0);
+
+
+}
