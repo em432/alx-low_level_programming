db3f63d5fcd223aad68f8eef54d1ada30df48bdc
em432 <emejeothniel@gmail.com>
2022-03-12 03:03:51 +0000

    My commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int number_left;
 int number_right;
 
 
-for (number_left = 48; number_right <= 57; number_left++)
+for (number_left = 48; number_right <= 121; number_left++)
 {
-for (number_right = number_left + 1 ; number_right <= 57; number_right++)
+for (number_right = number_left + 1 ; number_right <= 121; number_right++)
 {
 
 putchar(number_left);
 putchar (number_right);
 
 
-if ((number_left == 56) && (number_right == 57))
+if ((number_left == 56) && (number_right == 121))
 {
 break;
 }
