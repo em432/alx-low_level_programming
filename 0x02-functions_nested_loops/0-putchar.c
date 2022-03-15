2790946eadf733880b7994299e28f7868da35b4c
em432 <emejeothniel@gmail.com>
2022-03-15 07:30:52 +0000

    my commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On sucess/correct 1.
+ * On error, returne an error
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My taskes for 0x02-functions_nested_loops
