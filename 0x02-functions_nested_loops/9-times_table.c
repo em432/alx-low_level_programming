6807cdb1e9938a20434c4c4aa6187ee79aaff0c3
em432 <emejeothniel@gmail.com>
2022-03-15 13:38:39 +0000

    my commit message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table -prints tables
  *
@@ -31,15 +30,11 @@ if (b < 9)
 _putchar(',');
 _putchar (' ');
 }
-
 }
 else
 {
-
-
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
-
 if (b < 9)
 {
 _putchar(',');
