4c091c166e0a7be9a379595bb20b95d3fad02a63
em432 <emejeothniel@gmail.com>
2022-03-15 08:03:49 +0000

    My commit messsage

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * 
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
