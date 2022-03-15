88bbb1310ec6440654f773eb42c5fb3d8fb2190e
em432 <emejeothniel@gmail.com>
2022-03-15 12:45:41 +0000

    my commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,16 +9,16 @@
 
 int main(void)
 {
-         _putchar('_');
-         _putchar('p');
-         _putchar('u');
-         _putchar('t');
-         _putchar('c');
-         _putchar('h');
-         _putchar('a');
-         _putchar('r');
-         _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-         return (0);
+return (0);
 }
 
