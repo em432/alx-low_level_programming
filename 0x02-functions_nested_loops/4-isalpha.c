1ed73546894607c8eaa8c99391e5f29ffd15fa9b
em432 <emejeothniel@gmail.com>
2022-03-15 13:07:58 +0000

    my commit message

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - Return 1 if c is a letter. lowercase or uppercase 
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
+ *@c: The int to print
  * Return: Always 0.
  */
 
