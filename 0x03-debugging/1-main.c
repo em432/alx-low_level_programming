abde07c3b3d3f5c8f987f3d566fa0f0ee9142d46
em432 <emejeothniel@gmail.com>
2022-03-16 08:20:50 +0000

    my commit message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*
-* while (i < 10)
-* {
-* Infinite loop - No increase of variable
-* putchar(i);
-*
-* }
+*while (i < 10)
+*{
+*Infinite loop - No increase of variable
+*putchar(i);
+}
 */
 
 
