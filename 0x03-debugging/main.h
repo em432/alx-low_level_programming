4f4e76a45b317a9c90cacb46630ae0d5c258b682
em432 <emejeothniel@gmail.com>
2022-03-16 09:00:29 +0000

    my commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-
+void  print_remaining_days(int month, int day, int year);
 #endif
