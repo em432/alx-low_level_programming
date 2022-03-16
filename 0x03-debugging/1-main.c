1b532faac822f2de44218f01fc10128690fdbafd
em432 <emejeothniel@gmail.com>
2022-03-16 08:14:29 +0000

    my message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,13 +13,12 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 
-/*
- *while (i < 10)
- *{
- *Infinite loop - No increase of variable
- *putchar(i);
+while (i < 10)
+{
+
+putchar(i);
 }
-*/
+
 
 printf("Infinite loop avoided! \\o/\n");
 return (0);
