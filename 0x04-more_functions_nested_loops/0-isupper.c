0400aa3803310753ffa9ebb129417456d5710759
em432 <emejeothniel@gmail.com>
2022-03-17 14:36:19 +0000

    my commit message

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
