baa87b54ffda007cc16ff3d8bdb2f4ee5cc4920a
em432 <emejeothniel@gmail.com>
2022-03-17 15:12:37 +0000

    my commit message

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+while (i < 10)
+{
+_putchar (1 + '0');
+i++;
+}
+
+_putchar ('\n');
+}
