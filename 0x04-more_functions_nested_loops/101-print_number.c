fa503653aef616a0422734ab15ecb76c065d712b
em432 <emejeothniel@gmail.com>
2022-03-18 00:27:07 +0000

    my commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
 * print_number - that prints an integer
-*@n
+*@n: print int
 * Return: Always 0.
 */
 void print_number(int n)
 {
-int a = 98;
-int b = 402;
-int c = 1024;
-int d = 0;
-int e = -98;
-
-_putchar (a , b , c , d , e );
-}
-_putchar ('\n');
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+return (0);
 }
