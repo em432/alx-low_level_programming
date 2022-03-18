1dd633316e4a1acce952a372e56353610a1c301c
em432 <emejeothniel@gmail.com>
2022-03-18 00:21:36 +0000

    my commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_number - that prints an integer
+*@n
+* Return: Always 0.
+*/
+void print_number(int n)
+{
+int a = 98;
+int b = 402;
+int c = 1024;
+int d = 0;
+int e = -98;
+
+_putchar (a , b , c , d , e );
+}
+_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_number(int n);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
