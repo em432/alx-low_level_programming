7a0182b48473443dcb01a2bd747fb31c9c56cfeb
em432 <emejeothniel@gmail.com>
2022-03-18 00:28:35 +0000

    my commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,3 @@
 */
 void print_number(int n)
 {
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
-}
