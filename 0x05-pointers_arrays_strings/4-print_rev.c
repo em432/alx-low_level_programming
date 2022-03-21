ea2ea8ba11f396cf4f98baf8ebb03974f8f7d6ca
em432 <emejeothniel@gmail.com>
2022-03-21 16:07:31 +0000

    my commit message

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (i = i - 1 ; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+
+_putchar ('\n');
+}
