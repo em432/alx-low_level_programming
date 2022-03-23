823833774d89f3e8585bc2270d8332f7e5b1ce41
em432 <emejeothniel@gmail.com>
2022-03-23 19:20:25 +0000

    my commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
+char *string_toupper(char *str);
 char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
