9a0275f72f3142d8c6d292d2dd6353de405deb96
em432 <emejeothniel@gmail.com>
2022-03-25 04:20:32 +0100

    my commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,34 +9,35 @@
  */
 void print_buffer(char *b, int size)
 {
-int j, k, l;
-if (size <= 0)
-printf("\n");
-else
-{
-for (j = 0; j < size; j += 10)
-{
-printf("%.8x:", j);
-for (k = j; k < j + 10; k++)
-{
-if (k % 2 == 0)
-printf(" ");
-if (k < size)
-printf("%.2x", *(b + k));
-else
-printf("  ");
-}
-printf(" ");
-for (l = j; l < j + 10; l++)
-{
-if (l >= size)
-break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
-else
-printf("%c", *(b + l));
-}
-printf("\n");
-}
-}
+	int j, k, l;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
 }
