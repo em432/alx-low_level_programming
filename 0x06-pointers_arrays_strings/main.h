c8783440090544cb43a90eb13493ff678347f174
em432 <emejeothniel@gmail.com>
2022-03-25 01:44:01 +0000

    my commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int _putchar(char c);
+int _putchar(char);
 char *leet(char *);
