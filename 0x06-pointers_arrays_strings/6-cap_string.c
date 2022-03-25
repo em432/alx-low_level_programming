91033277b30d7001377f27a57cf024b299faa25c
em432 <emejeothniel@gmail.com>
2022-03-25 00:41:16 +0000

    my commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ int count = 0, i;
 int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) -32;
+*(s + count) = *(s + count) - 32;
 count++;
 while (*(s + count) != '\0')
 {
