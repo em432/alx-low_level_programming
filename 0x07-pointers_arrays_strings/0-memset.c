9c3db80d0e31d47d8e2cb6722837bb06aabd3afd
em432 <emejeothniel@gmail.com>
2022-03-28 19:08:31 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,23 @@
-#include "main.sshh"
-
+#include "main.h"
 /**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
- * Return: *s
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 	unsigned int i;
 
+	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
-	}
+		*(s + i) = b; /*add 1 position s*/
+
+	} /*END FOR*/
+
 	return (s);
 }
