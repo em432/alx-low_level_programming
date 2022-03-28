989a95946a7e03198f587362544a6a9d53d9d458
em432 <emejeothniel@gmail.com>
2022-03-28 18:41:34 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	/*Delacring FOR*/
+	
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
+		*(s + i) = b; 
 
-	} /*END FOR*/
+	} 
 	return (s);
 }
