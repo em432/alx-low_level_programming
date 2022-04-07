3ead3de32cb0e02915432fec26f055083bb4adff
em432 <emejeothniel@gmail.com>
2022-04-07 21:47:08 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,69 +1,51 @@
 #include "main.h"
-/**
- *_strlen - count array
- *@s: array of elements
- *Return: int i
- */
-int _strlen(char *s)
-{
-unsigned int i;
-
-i = 0;
-while (s[i] != '\0') /*Count charater of string*/
-{
-i++;
-}
-return (i);
-}
 
 /**
- *string_nconcat - concatenates two strings
- *@s1: string
- *@s2: string
- *@n: number bytes
- *Return: char dst
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *dst;
-unsigned int i, count, j, size, size_1, size_2;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-if (s1 == NULL) /*If the array is empty*/
-s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-if (s2 == NULL)
-s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-size_1 = _strlen(s1);
-size_2 = _strlen(s2);
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
 
-if (n >= size_2)
-{
-count = size_2;
-}
-else
-{
-count = n;
-}
-/*count size total*/
-size = size_1 + count + 1;
-/*malloc*/
-dst = (char *) malloc(size * sizeof(char));
-if (dst == 0)
-{
-return (NULL);
-}
-/*Concantenate arrays*/
-{
-for (i = 0; *(s1 + i) != '\0'; i++)
-*(dst + i) = *(s1 + i);
-for (j = 0; j < count; j++)
-{
-*(dst + i) = *(s2 + j);
-i++;
-}
-*(dst + i) = '\0';
-}
-return (dst);
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
